216848f720ff0714b46468feb347f1bdd89660d9
Schalk Neethling <schalkneethling@Schalks-MacBook-Pro.local>
2025-03-24 00:21:57 +0200

    let us launch this thing

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,8 +6,12 @@ export default function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 
   eleventyConfig.addPassthroughCopy("css/*.css");
+  eleventyConfig.addPassthroughCopy("assets/js/*.js");
   eleventyConfig.addPassthroughCopy("assets/typography/*.woff2");
   eleventyConfig.addPassthroughCopy("assets/*.svg");
+  eleventyConfig.addPassthroughCopy("assets/*.png");
+  eleventyConfig.addPassthroughCopy("assets/*.ico");
+  eleventyConfig.addPassthroughCopy("assets/*.webmanifest");
   // the default is "copy"
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 }
